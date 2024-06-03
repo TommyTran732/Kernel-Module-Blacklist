f7fbb01a0c9d046e317566c293f51ff848f2e48c
Tommy <contact@tommytran.io>
2024-06-02 21:47:06 -0700

    Update driver blacklists
    Signed-off-by: Tommy <contact@tommytran.io>

--- kmod-blacklist/generate-kmod-blacklist-aggregate.sh
+++ kmod-blacklist/generate-kmod-blacklist-aggregate.sh
@@ -14,8 +14,30 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
+output(){
+    echo -e '\e[36m'"$1"'\e[0m';
+}
+
+dataset_selection() {
+    output 'Which dataset do you want to generate the kmod blacklist for?'
+    output '1) VPS'
+    output '2) Workstation'
+    output 'Insert the number of your selection:'
+    read -r choice
+    case $choice in
+        1 ) dataset='vps'
+            ;;
+        2 ) dataset='workstation'
+            ;;
+        * ) output 'You did not enter a valid selection.'
+            dataset_selection
+    esac
+}
+
+dataset_selection
+
 # Combine all sample data for available
-sort -u sample-data/vps/available/* > blacklist.txt
+sort -u sample-data/"${dataset}"/available/* > blacklist.txt
 
 # Combine all sample data for necessary
 sort -u sample-data/vps/necessary/* > necessary.txt
@@ -40,7 +62,7 @@ sed -i '/^$/d' blacklist.txt
 
 # Create final blacklist config
 while read -r KMOD; do
-echo "install ${KMOD} /bin/false" >> blacklist.conf
+echo "install ${KMOD} /bin/false" >> ${dataset}-blacklist.conf
 done < blacklist.txt
 
 # Cleanup
